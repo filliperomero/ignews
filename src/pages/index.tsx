558a02331ebd74b60be1d02fa51d18917ab8d751
filliperomero <fillipe.romero@gmail.com>
2021-09-26 17:46:42 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <div>
+        <h1>Home Page</h1>
+      </div>
+    </>
+  )
+}
+
+export default Home
